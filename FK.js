7ae5efee8eda407f932b88ed2daa0a246b4c79ee
carhope <26_61@bssm.hs.kr>
2026-06-09 19:58:19 +0900

    버튼 onclick으로 페이지 연결

--- FK.js
+++ FK.js
@@ -2,3 +2,7 @@ const loginBth = document.querySelector('.login-bth');
 function isLogin(){
     window.location.href='../html/FK_login.html'
 };
+
+function GoToPage(pageURL){
+    window.location.href=pageURL;
+}
